03d621496ffb7f1684c2c50c8f6b957412dabd55
fhyfirman <firman@hungryhub.com>
2023-02-09 18:10:26 +0700

    add figaro for manage env

--- app/services/kafka_producer.rb
+++ app/services/kafka_producer.rb
@@ -1,8 +1,19 @@
 require 'kafka'
 
 class KafkaProducer
+  CLIENT_KEY = Figaro.env.kafka_client_key.to_s
+  CLIENT_CERT = Figaro.env.kafka_client_cert.to_s
+  CLIENT_CA = Figaro.env.kafka_ca_cert.to_s
+
   def initialize
-    @kafka = Kafka.new([Figaro.env.kafka_host])
+    kafka_host = Figaro.env.kafka_host
+    kafka_topic = Figaro.env.kafka_topic
+
+    client_key = File.read(CLIENT_KEY)
+    client_cert = File.read(CLIENT_CERT)
+    ca_cert = IO.read(CLIENT_CA)
+
+    @kafka = Kafka.new([kafka_host], ssl_client_cert: client_cert, ssl_client_cert_key: client_key, ssl_ca_cert: ca_cert)
   end
 
   def produce(topic, message)
